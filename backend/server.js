28f719741d51c3f61269395468b8a838942cfe8f
violetsoko <148396994+violetsoko@users.noreply.github.com>
2025-12-18 16:31:07 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const trackRoutes = require("./routes/track");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/track", trackRoutes);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
