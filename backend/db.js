b33e7acd3de9f262715088e30e85d8c82c2557ce
violetsoko <148396994+violetsoko@users.noreply.github.com>
2025-12-18 16:31:57 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,17 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./passport.db");
+
+db.run(`
+CREATE TABLE IF NOT EXISTS applications (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  application_number TEXT,
+  full_name TEXT,
+  dob TEXT,
+  status TEXT,
+  office TEXT
+)
+`);
+
+module.exports = db;
 
